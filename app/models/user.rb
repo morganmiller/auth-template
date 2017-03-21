b181b236f85a012dd0c63071ad0ee9d031eda9af
Morgan Miller <m.miller722@gmail.com>
2017-03-21 16:11:12 -0600

    adds pundit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  enum role: [:user, :admin]
+
+  def admin?
+    role == :admin
+  end
 end
